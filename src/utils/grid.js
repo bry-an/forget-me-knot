1cab69c55d2a053808371a1aeb0811659df44a97
Bryan Yunis <bryany@booj.com>
2021-04-18 01:32:44 -0600

    modify grid utility and test

--- src/utils/grid.js
+++ src/utils/grid.js
@@ -2,6 +2,10 @@ import assoc from "ramda/src/assoc";
 import compose from "ramda/src/compose";
 import concat from "ramda/src/concat";
 import reduce from "ramda/src/reduce";
+import curry from "ramda/src/curry";
+import not from "ramda/src/not";
+import filter from "ramda/src/filter";
+import propEq from "ramda/src/propEq";
 
 const tiePair = (pair) => {
   const [pair1, pair2] = pair;
@@ -16,4 +20,8 @@ const addTileToGrid = (grid, tile) => concat(generateLinkedItems(tile), grid);
 
 const buildGrid = reduce(addTileToGrid, []);
 
-export { buildGrid };
+const notSlug = (slug) => compose(not, propEq("slug", slug));
+
+const removeFrom = curry((grid, slug) => filter(notSlug(slug), grid));
+
+export { buildGrid, removeFrom };
--- src/utils/grid.spec.js
+++ src/utils/grid.spec.js
@@ -1,6 +1,7 @@
-import { buildGrid } from "./grid.js";
+import { buildGrid, removeFrom } from "./grid.js";
 
 const sampleGridItems = ["test1", "test2"];
+const sampleGrid = [{ slug: "test1" }, { slug: "test2" }];
 
 describe("buildGrid", () => {
   it("Properly builds grid", () => {
@@ -11,5 +12,17 @@ describe("buildGrid", () => {
       { slug: "test1", sibling: { slug: "test1" } },
       { slug: "test1", sibling: { slug: "test1" } },
     ]);
+  });
+  test("Siblings are references to actual tiles", () => {
+    const grid = buildGrid([{ slug: "test" }]);
+    expect(grid[0].sibling === grid[1]);
+  });
+});
+
+describe("removeFrom", () => {
+  it("Properly removes tiles from grid", () => {
+    const removeSlugFromGrid = removeFrom(sampleGrid);
+    const filteredGrid = removeSlugFromGrid("test1");
+    expect(filteredGrid).toStrictEqual([{ slug: "test2" }]);
   });
 });
