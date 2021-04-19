d1d28dc71720a3f96814d41fe5f223797442a010
Bryan Yunis <bryany@booj.com>
2021-04-19 02:06:09 -0600

    fix url error

--- src/api/giphyClient.js
+++ src/api/giphyClient.js
@@ -1,6 +1,6 @@
 import { buildRequest } from "./index.js";
 
-const baseGiphySearchUrl = `https://api.giphy.com/v1/gifs/?type=search&rating=g&api_key=${process.env.VUE_APP_KEY}`;
+const baseGiphySearchUrl = `https://api.giphy.com/v1/gifs/search?rating=g&api_key=${process.env.VUE_APP_API_KEY}`;
 const giphyRequest = buildRequest(baseGiphySearchUrl);
 const fetchImagesByQuery = giphyRequest(fetch);
 
