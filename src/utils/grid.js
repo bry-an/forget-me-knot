fe6f5e1eaf4df73132db9fdf385ff7a0887050ec
Bryan Yunis <bryany@booj.com>
2021-04-19 02:03:29 -0600

    Make grid unreactive
    Adding reactivity to the grid adds unnecessary complexity. A normal array can provide the same functionality without having to extract raw values from the proxy

--- src/utils/grid.js
+++ src/utils/grid.js
@@ -2,7 +2,6 @@ import assoc from "ramda/src/assoc";
 import compose from "ramda/src/compose";
 import concat from "ramda/src/concat";
 import reduce from "ramda/src/reduce";
-import curry from "ramda/src/curry";
 import not from "ramda/src/not";
 import filter from "ramda/src/filter";
 import propEq from "ramda/src/propEq";
@@ -12,7 +11,10 @@ const tiePair = (pair) => {
   return [assoc("sibling", pair2, pair1), assoc("sibling", pair1, pair2)];
 };
 
-const generatePair = (slug) => [{ slug }, { slug }];
+const generatePair = (slug) => [
+  { slug, key: `${slug}-1`, clicked: false },
+  { slug, key: `${slug}-2`, clicked: false },
+];
 
 const generateLinkedItems = compose(tiePair, generatePair);
 
@@ -22,6 +24,6 @@ const buildGrid = reduce(addTileToGrid, []);
 
 const notSlug = (slug) => compose(not, propEq("slug", slug));
 
-const removeFrom = curry((grid, slug) => filter(notSlug(slug), grid));
+const removeFromGrid = (slug, grid) => filter(notSlug(slug), grid);
 
-export { buildGrid, removeFrom };
+export { buildGrid, removeFromGrid };
